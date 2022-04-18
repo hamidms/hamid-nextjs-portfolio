0458805abd7833772fb17b84e9edf4e70e3f144b
hamidms <hamidmsukardi@gmail.com>
2022-04-19 03:38:46 +0700

    adding header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './Header.module.css'
 
 export default function Header() {
   return (
-    <div>
-        
+    <div className='text-center'>
+        <h1>Hamid Machfudin Sukardi</h1>
+        <p>Thank you for visiting. This is my personal page.</p>
     </div>
   )
 }
